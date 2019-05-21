625b839503f669ae3763d08f6f7cd124911c2d47
Maxime Derouet <mxderouet@gmail.com>
2019-05-21 16:00:13 +0200

    tasklist

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "tasks", to: "tasks#index", as: :tasks
+  get "tasks/new", to: "tasks#new", as: :new
+  get "tasks/edit/:id", to: "tasks#edit", as: :edit
+  get "tasks/:id", to: "tasks#show", as: :task
+  patch "tasks/:id", to: "tasks#update"
+  post "tasks", to: "tasks#create"
+  delete "tasks/:id", to: "tasks#destroy"
 end
